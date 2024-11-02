2e520c20aa27e563df0f49c5b527053a69fbbdc1
HP <HP@DESKTOP-NMBILJD>
2024-11-02 19:42:20 +0100

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+	
+
+
+
+
+
+
+int main() {
+	int i = 1;
+	int p = 0;
+	int n;
+	int x;
+	cout << "num n :" << endl;
+	cin >> n;
+
+	cout << "num x :" << endl;
+	cin >> x;
+
+	while (i <= p)
+	{
+		cout << pow(n, x) << endl;
+		i++;
+	}
+
+
+
+;
+
+	return 0;
+}
